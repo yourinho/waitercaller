84184a298f5d2fd7176d9c77cc93518f5fd8cdf8
yuragasyuk <yura.gasyuk@gmail.com>
2018-01-21 00:39:39 +0300

    Fix some indentations.

--- dbhelper.py
+++ dbhelper.py
@@ -16,37 +16,37 @@ class DBHelper:
     def get_user(self, email):
 		return self.db.users.find_one({"email": email})
 
-	def add_user(self, email, salt, hashed):
+    def add_user(self, email, salt, hashed):
 		self.db.users.insert({"email": email, "salt": salt, "hashed": hashed})
 
 	# Tables.
-	def add_table(self, number, owner):
+    def add_table(self, number, owner):
 		new_id = self.db.tables.insert({"number": number, "owner": owner})
 		return new_id
 
-	def update_table(self, _id, url):
+    def update_table(self, _id, url):
 		self.db.tables.update({"_id": _id}, {"$set": {"url": url}})
 
-	def get_tables(self, owner_id):
+    def get_tables(self, owner_id):
 		return list(self.db.tables.find({"owner": owner_id}))
 
-	def get_table(self, table_id):
+    def get_table(self, table_id):
 		return self.db.tables.find_one({"_id": ObjectId(table_id)})
 
-	def delete_table(self, table_id):
+    def delete_table(self, table_id):
 		self.db.tables.remove({"_id": ObjectId(table_id)})
 
 	# Requests.
-	def add_request(self, table_id, time):
+    def add_request(self, table_id, time):
 		table = self.get_table(table_id)
 		self.db.requests.insert({"owner": table['owner'],
 			"table_number": table['number'],
 			"table_id": table_id,
 			"time": time})
 
-	def get_requests(self, owner_id):
+    def get_requests(self, owner_id):
 		return list(self.db.requests.find({"owner": owner_id}))
 
-	def delete_request(self, request_id):
+    def delete_request(self, request_id):
 		self.db.requests.remove({"_id": ObjectId(request_id)})
-		
+
